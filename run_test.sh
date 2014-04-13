cd6601fc2e9ae569b03b3d7e20ef9e73e1cbc51a
kakwa <carpentier.pf@gmail.com>
2014-04-13 23:43:40 +0200

    adding start and stop for the dnscherryd daemon

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 cd `dirname $0`
+#deploying basic configuration (dns+zone+key) and dnscherry.ini
 ./tests/cfg/deploy.sh
+
+#starting dnscherryd
+dnscherryd -d -p /tmp/dnscherry.pid -c /etc/dnscherry/dnscherry.ini
+
+#stoping dnscherryd
+kill `cat /tmp/dnscherry.pid`
