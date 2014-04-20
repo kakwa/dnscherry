675c93b536f674caa0b8ad47fdf7bd8879189917
kakwa <carpentier.pf@gmail.com>
2014-04-20 15:41:30 +0200

    adding the basic struct for unit tests

---
+++ tests/test_form.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+import sys
+
+import dnscherry
+import mechanize
+
+def add_record(br, key, value, ttl, rtype):
+    pass
+
+def del_records(br, key, value, ttl, rtype):
+    pass
+
+def get_zone(zone, key, host):
+    pass
+
+class TestForm(object):
+
+        def del_records(self):
+            pass
+
+        def del_none_existing_record(self):
+            pass
+
+        def get_record(self):
+            pass
+
+        def get_wrong_zone(self):
+            pass
+
+        def add_faulty_record(self):
+            pass
+
+        def add_record(self):
+            pass
+#            assert res == expected
+
