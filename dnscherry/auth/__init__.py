daf0baae72cacbed8a5a1dcbc6feb7ebbb489816
kakwa <carpentier.pf@gmail.com>
2014-05-02 11:37:01 +0200

    adding layout for auth modules
    * adding layout for auth module
    * adding none.py module

---
+++ dnscherry/auth/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# vim:set expandtab tabstop=4 shiftwidth=4:
+
+# Form based authentication for CherryPy. Requires the
+# Session tool to be loaded.
+#
+
+import cherrypy
+
+SESSION_KEY = '_cp_username'
+
+class Auth(object):
+
+    def __init__(self, config):
+        pass
+
+    def check_credentials(self, username, password):
+        return True
+
+    def check_auth(self):
+        return True
+
+    def end_session(self):
+        sess = cherrypy.session
+        username = sess.get(SESSION_KEY, None)
+        sess[SESSION_KEY] = None
+        if username:
+            cherrypy.request.login = None
+
---
+++ dnscherry/auth/none.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# vim:set expandtab tabstop=4 shiftwidth=4:
+
+import cherrypy
+import dnscherry.auth
+
+class Auth(dnscherry.auth.Auth):
+
+    def __init__(self, config):
+        # no need for a logout button
+        self.logout_button = False
+        if 'auth.user_header_name' in config:
+            self.user_header_name = config ['auth.user_header_name']
+        else:
+            self.user_header_name = None
+
+    def check_auth(self):
+        if self.user_header_name is None:
+            return 'unknown user'
+        else:
+            if self.user_header_name in cherrypy.request.headers:
+                return cherrypy.request.headers[self.user_header_name]
+            else:
+                raise cherrypy.HTTPError(
+                    "403 Forbidden", "You are not allowed to access this resource.")
