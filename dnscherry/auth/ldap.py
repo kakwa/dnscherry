ff15f241b616066d8b77b2bc0ff3aa9c92369ece
kakwa <carpentier.pf@gmail.com>
2014-05-03 17:29:41 +0200

    basis for ldap module (still need some work)

--- dnscherry/auth/ldap.py
+++ dnscherry/auth/ldap.py
@@ -2,24 +2,50 @@
 # vim:set expandtab tabstop=4 shiftwidth=4:
 
 import cherrypy
+import ldap
 import dnscherry.auth
+
+SESSION_KEY = '_cp_username'
 
 class Auth(dnscherry.auth.Auth):
 
     def __init__(self, config):
-        # no need for a logout button
-        self.logout_button = False
-        if 'auth.user_header_name' in config:
-            self.user_header_name = config ['auth.user_header_name']
-        else:
-            self.user_header_name = None
+        self.logout_button = True
+        self.userdn = config['auth.ldap.userdn']
+        self.loginattribut = config['auth.ldap.loginattribut']
+        self.groupdn = config['auth.ldap.groupdn']
+        self.groupattribut = config['auth.ldap.groupattribut']
+        self.binddn = config['auth.ldap.binddn']
+        self.bindpassword = config['auth.ldap.bindpassword']
+        self.uri = config['auth.ldap.uri']
+        self.ca = config['auth.ldap.ca']
+        self.starttls = config['auth.ldap.starttls']
+        self.checkcert = config['auth.ldap.checkcert']
+
+    def check_credentials(self, username, password):
+        username = re.sub(',.*','', username)
+        user_dn_entry = '%(loginattr)=%(user)s,%(userdn)' % {
+                'loginattr': self.loginattribut,
+                'user': username,
+                'userdn': self.userdn
+                }
+        try:
+            # build a client
+            ldap_client = ldap.initialize(self.uri)
+            # perform a synchronous bind
+            ldap_client.set_option(ldap.OPT_REFERRALS, 0)
+            ldap_client.simple_bind_s(user_dn_entry, password)
+            return True
+        except ldap.INVALID_CREDENTIALS:
+            ldap_client.unbind()
+            return False
+        except ldap.SERVER_DOWN:
+            raise cherrypy.HTTPError(
+                "500", "Ldap serveur unavailable")
 
     def check_auth(self):
-        if self.user_header_name is None:
-            return 'unknown user'
+        username = cherrypy.session.get(SESSION_KEY)
+        if username:
+           return username
         else:
-            if self.user_header_name in cherrypy.request.headers:
-                return cherrypy.request.headers[self.user_header_name]
-            else:
-                raise cherrypy.HTTPError(
-                    "403 Forbidden", "You are not allowed to access this resource.")
+           raise cherrypy.HTTPRedirect("/signin")
