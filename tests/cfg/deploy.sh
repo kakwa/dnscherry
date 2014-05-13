25e38915dce5b30892822c7d18677eeacc351bab
kakwa <carpentier.pf@gmail.com>
2014-05-13 19:42:37 +0200

    update test env

--- tests/cfg/deploy.sh
+++ tests/cfg/deploy.sh
@@ -7,3 +7,7 @@ cd `dirname $0`/../../
 sudo sed -i "s%template_dir.*%template_dir = '`pwd`/resources/templates/'%" /etc/dnscherry/dnscherry.ini
 sudo sed -i "s%tools.staticdir.dir.*%tools.staticdir.dir = '`pwd`/resources/static/'%" /etc/dnscherry/dnscherry.ini
 
+sudo chown -R openldap:openldap /etc/ldap/
+sudo /etc/init.d/slapd restart
+ldapadd -H ldap://localhost -x -D "cn=admin,dc=example,dc=org" -f /etc/ldap/content.ldif -w password
+sed -i "s/\(127.0.0.1.*\)/\1 ldap.dnscherry.org/"
