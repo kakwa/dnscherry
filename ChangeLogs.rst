ed5fbb43f26869add0347bc9bd01c30485b5456c
kakwa <carpentier.pf@gmail.com>
2015-11-09 00:29:19 +0100

    version bump

--- ChangeLogs.rst
+++ ChangeLogs.rst
@@ -1,6 +1,12 @@
 Changelog
 =========
 
+Version 0.1.1
+*************
+
+* fix configuration overwrite in setup.py
+* fix syslog message error
+
 Version 0.1.0
 *************
 
--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ if not os.path.exists(conffile):
 setup(
     name='dnscherry',
     zip_safe=False,
-    version='0.1.0',
+    version='0.1.1',
     author='Pierre-Francois Carpentier',
     author_email='carpentier.pf@gmail.com',
     packages=['dnscherry', 'dnscherry.auth'],
