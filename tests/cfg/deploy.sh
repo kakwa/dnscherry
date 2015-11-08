e5d76789539e68f610cd7e9cbc1edc8260e9a800
kakwa <carpentier.pf@gmail.com>
2015-11-08 23:31:54 +0100

    clean default ldap configuration

--- tests/cfg/deploy.sh
+++ tests/cfg/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo DEBIAN_FRONTEND=noninteractive aptitude install bind9 -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  -f -q -y
+sudo DEBIAN_FRONTEND=noninteractive aptitude install slapd -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  -f -q -y
 sudo rsync -a `dirname $0`/ /
 sudo chown -R bind:bind /etc/bind/
 sudo /etc/init.d/bind9 restart
@@ -8,6 +10,8 @@ sudo sed -i "s%template_dir.*%template_dir = '`pwd`/resources/templates/'%" /etc
 sudo sed -i "s%tools.staticdir.dir.*%tools.staticdir.dir = '`pwd`/resources/static/'%" /etc/dnscherry/dnscherry.ini
 
 sudo chown -R openldap:openldap /etc/ldap/
+rm '/etc/ldap/slapd.d/cn=config/olcDatabase={1}mdb.ldif'
+rm '/etc/ldap/slapd.d/cn=config/olcBackend={0}mdb.ldif'
 sudo /etc/init.d/slapd restart
 ldapadd -H ldap://localhost -x -D "cn=admin,dc=example,dc=org" -f /etc/ldap/content.ldif -w password
 sed -i "s/\(127.0.0.1.*\)/\1 ldap.dnscherry.org/" /etc/hosts
